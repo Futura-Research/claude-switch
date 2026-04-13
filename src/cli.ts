79bfd078380661ed50d12f40ee4ff1f46b06ed47
eapenzacharias <eapenzac@gmail.com>
2026-04-13 22:59:13 +0530

    chore: bump version to 1.0.0

--- src/cli.ts
+++ src/cli.ts
@@ -4,7 +4,7 @@ import { addRule, removeRule, listRules } from "./rules.js";
 import { resolveProfile, parseArgs } from "./resolver.js";
 import { launch } from "./launcher.js";
 
-const VERSION = "0.0.0";
+const VERSION = "1.0.0";
 
 export function printUsage(): void {
   console.log(
